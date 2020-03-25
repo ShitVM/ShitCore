633ce057b40f748f282ed7eb925ef4af2d7d24b8
kmc7468 <kmc7468@naver.com>
2020-03-25 20:06:20 +0900

    TypeInfo, Type, Object 클래스 추가

---
+++ include/svm/Type.hpp
@@ -0,0 +1,62 @@
+#pragma once
+
+#include <svm/detail/ReferenceWrapper.hpp>
+
+#include <cstddef>
+#include <cstdint>
+#include <string>
+
+namespace svm {
+	enum class TypeCode : std::uint32_t {
+		None,
+
+		Int = 3,
+		Long,
+		Double = 6,
+		Pointer,
+		GCPointer,
+		Array,
+		Structure = 20,
+	};
+
+	class TypeInfo final {
+	public:
+		std::string Name;
+		TypeCode Code = TypeCode::None;
+		std::size_t Size = 0;
+
+	public:
+		TypeInfo() noexcept = default;
+		TypeInfo(std::string name, TypeCode code) noexcept;
+		TypeInfo(std::string name, TypeCode code, std::size_t size) noexcept;
+		TypeInfo(TypeInfo&& typeInfo) noexcept;
+		~TypeInfo() = default;
+
+	public:
+		TypeInfo& operator=(TypeInfo&& typeInfo) noexcept;
+		bool operator==(const TypeInfo&) = delete;
+		bool operator!=(const TypeInfo&) = delete;
+	};
+}
+
+namespace svm {
+	class Type final : public detail::ReferenceWrapper<TypeInfo> {
+	public:
+		using detail::ReferenceWrapper<TypeInfo>::ReferenceWrapper;
+
+	public:
+		bool IsFundamentalType() const noexcept;
+		bool IsPointer() const noexcept;
+		bool IsArray() const noexcept;
+		bool IsStructure() const noexcept;
+		bool IsValidType() const noexcept;
+	};
+
+	extern const Type NoneType;
+	extern const Type IntType;
+	extern const Type LongType;
+	extern const Type DoubleType;
+	extern const Type PointerType;
+	extern const Type GCPointerType;
+	extern const Type ArrayType;
+}
---
+++ src/Type.cpp
@@ -0,0 +1,59 @@
+#include <svm/Type.hpp>
+
+#include <svm/Object.hpp>
+
+#include <utility>
+
+namespace svm {
+	TypeInfo::TypeInfo(std::string name, TypeCode code) noexcept
+		: Name(std::move(name)), Code(code) {}
+	TypeInfo::TypeInfo(std::string name, TypeCode code, std::size_t size) noexcept
+		: Name(std::move(name)), Code(code), Size(size) {}
+	TypeInfo::TypeInfo(TypeInfo&& typeInfo) noexcept
+		: Name(std::move(typeInfo.Name)), Code(typeInfo.Code), Size(typeInfo.Size) {}
+
+	TypeInfo& TypeInfo::operator=(TypeInfo&& typeInfo) noexcept {
+		Name = std::move(typeInfo.Name);
+		Code = typeInfo.Code;
+		Size = typeInfo.Size;
+		return *this;
+	}
+}
+
+namespace svm {
+	bool Type::IsFundamentalType() const noexcept {
+		const TypeCode code = GetReference().Code;
+		return TypeCode::None != code && code < TypeCode::Array;
+	}
+	bool Type::IsPointer() const noexcept {
+		const TypeCode code = GetReference().Code;
+		return code == TypeCode::Pointer || code == TypeCode::GCPointer;
+	}
+	bool Type::IsArray() const noexcept {
+		return GetReference().Code == TypeCode::Array;
+	}
+	bool Type::IsStructure() const noexcept {
+		return GetReference().Code >= TypeCode::Structure;
+	}
+	bool Type::IsValidType() const noexcept {
+		return GetReference().Code != TypeCode::None;
+	}
+
+	namespace {
+		static const TypeInfo s_NoneType("none", TypeCode::None, 0);
+		static const TypeInfo s_IntType("int", TypeCode::Int, sizeof(IntObject));
+		static const TypeInfo s_LongType("long", TypeCode::Long, sizeof(LongObject));
+		static const TypeInfo s_DoubleType("double", TypeCode::Double, sizeof(DoubleObject));
+		static const TypeInfo s_PointerType("pointer", TypeCode::Pointer, sizeof(PointerObject));
+		static const TypeInfo s_GCPointerType("gcpointer", TypeCode::GCPointer, sizeof(GCPointerObject));
+		static const TypeInfo s_ArrayType("array", TypeCode::Array, sizeof(ArrayObject));
+	}
+
+	const Type NoneType = s_NoneType;
+	const Type IntType = s_IntType;
+	const Type LongType = s_LongType;
+	const Type DoubleType = s_DoubleType;
+	const Type PointerType = s_PointerType;
+	const Type GCPointerType = s_GCPointerType;
+	const Type ArrayType = s_ArrayType;
+}
