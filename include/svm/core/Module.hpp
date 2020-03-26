28bdc85e31aaa93f55c27fbc302130de52e6b221
kmc7468 <kmc7468@naver.com>
2020-03-26 16:07:44 +0900

    core::ModuleInfo, Module 클래스 추가

---
+++ include/svm/core/Module.hpp
@@ -0,0 +1,50 @@
+#pragma once
+
+#include <svm/core/ByteFile.hpp>
+#include <svm/core/virtual/VirtualModule.hpp>
+#include <svm/detail/ReferenceWrapper.hpp>
+
+#include <variant>
+#include <vector>
+
+namespace svm::core {
+	template<typename FI>
+	class ModuleInfo {
+	public:
+		std::variant<std::monostate, ByteFile, VirtualModule<FI>> Module;
+
+	public:
+		ModuleInfo() noexcept = default;
+		ModuleInfo(ByteFile&& byteFile) noexcept;
+		ModuleInfo(VirtualModule<FI>&& virtualModule) noexcept;
+		ModuleInfo(ModuleInfo&& moduleInfo) noexcept;
+		~ModuleInfo() = default;
+
+	public:
+		ModuleInfo& operator=(ModuleInfo&& moduleInfo) noexcept;
+		bool operator==(const ModuleInfo&) = delete;
+		bool operator!=(const ModuleInfo&) = delete;
+
+	public:
+		bool IsEmpty() const noexcept;
+		bool IsByteFile() const noexcept;
+		bool IsVirtualModule() const noexcept;
+
+		std::string_view GetPath() const noexcept;
+		Structure GetStructure(std::uint32_t index) const noexcept;
+		std::uint32_t GetStructureCount() const noexcept;
+		std::variant<Function, VirtualFunction<FI>> GetFunction(std::uint32_t index) const noexcept;
+		std::uint32_t GetFunctionCount() const noexcept;
+	};
+}
+
+namespace svm::core {
+	template<typename FI>
+	class Module : public detail::ReferenceWrapper<ModuleInfo<FI>> {
+	public:
+		using detail::ReferenceWrapper<ModuleInfo<FI>>::ReferenceWrapper;
+	};
+
+	template<typename FI>
+	using Modules = std::vector<ModuleInfo<FI>>;
+}
---
+++ include/svm/core/detail/impl/Module.hpp
@@ -0,0 +1,72 @@
+#pragma once
+#include <svm/core/Module.hpp>
+
+#include <cassert>
+#include <utility>
+
+namespace svm::core {
+	template<typename FI>
+	ModuleInfo<FI>::ModuleInfo(ByteFile&& byteFile) noexcept
+		: Module(std::move(byteFile)) {}
+	template<typename FI>
+	ModuleInfo<FI>::ModuleInfo(VirtualModule<FI>&& virtualModule) noexcept
+		: Module(std::move(virtualModule)) {}
+	template<typename FI>
+	ModuleInfo<FI>::ModuleInfo(ModuleInfo&& moduleInfo) noexcept
+		: Module(std::move(moduleInfo.Module)) {}
+
+	template<typename FI>
+	ModuleInfo<FI>& ModuleInfo<FI>::operator=(ModuleInfo&& moduleInfo) noexcept {
+		Module = std::move(moduleInfo.Module);
+		return *this;
+	}
+
+	template<typename FI>
+	bool ModuleInfo<FI>::IsEmpty() const noexcept {
+		return std::holds_alternative<std::monostate>(Module);
+	}
+	template<typename FI>
+	bool ModuleInfo<FI>::IsByteFile() const noexcept {
+		return std::holds_alternative<ByteFile>(Module);
+	}
+	template<typename FI>
+	bool ModuleInfo<FI>::IsVirtualModule() const noexcept {
+		return std::holds_alternative<VirtualModule<FI>>(Module);
+	}
+
+	template<typename FI>
+	std::string_view ModuleInfo<FI>::GetPath() const noexcept {
+		assert(!IsEmpty());
+
+		if (IsByteFile()) return std::get<ByteFile>(Module).GetPath();
+		else return std::get<VirtualModule<FI>>(Module).GetPath();
+	}
+	template<typename FI>
+	Structure ModuleInfo<FI>::GetStructure(std::uint32_t index) const noexcept {
+		assert(!IsEmpty());
+
+		if (IsByteFile()) return std::get<ByteFile>(Module).GetStructures()[index];
+		else return std::get<VirtualModule<FI>>(Module).GetStructures()[index];
+	}
+	template<typename FI>
+	std::uint32_t ModuleInfo<FI>::GetStructureCount() const noexcept {
+		assert(!IsEmpty());
+
+		if (IsByteFile()) return static_cast<std::uint32_t>(std::get<ByteFile>(Module).GetStructures().size());
+		else return static_cast<std::uint32_t>(std::get<VirtualModule<FI>>(Module).GetStructures().size());
+	}
+	template<typename FI>
+	std::variant<Function, VirtualFunction<FI>> ModuleInfo<FI>::GetFunction(std::uint32_t index) const noexcept {
+		assert(!IsEmpty());
+
+		if (IsByteFile()) return std::get<ByteFile>(Module).GetFunctions()[index];
+		else return std::get<VirtualModule<FI>>(Module).GetFunctions()[index];
+	}
+	template<typename FI>
+	std::uint32_t ModuleInfo<FI>::GetFunctionCount() const noexcept {
+		assert(!IsEmpty());
+
+		if (IsByteFile()) return static_cast<std::uint32_t>(std::get<ByteFile>(Module).GetFunctions().size());
+		else return static_cast<std::uint32_t>(std::get<VirtualModule<FI>>(Module).GetFunctions().size());
+	}
+}
